4fd1d2b25e9fda0c7b6eeefb21471ac15da30925
yogisaputro <saputro.yogi@rocketmail.com>
2013-11-28 19:17:48 +0700

    Create index.php
    file utama dari web service tugas 2

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+	$xmlfile = simplexml_load_file("cd_catalog.xml");
+	// htmlspecialchars($_GET["kolom"]);
+	header("content-type: text/xml");
+	if (htmlspecialchars($_GET["kolom"])=='all'){
+		echo "<".$xmlfile->getName().">";
+		foreach ($xmlfile->children() as $children1){
+		 	echo "<".$children1->getName().">";
+		 		foreach ($children1->children() as $children2) {
+		 			echo "<".$children2->getName().">".$children2."</".$children2->getName().">";
+		 		}
+		 	echo "</".$children1->getName().">";
+		}
+		echo "</".$xmlfile->getName().">";
+	}
+	elseif (htmlspecialchars($_GET["kolom"])=='basic'){
+		echo "<".$xmlfile->getName().">";
+		foreach ($xmlfile->children() as $children1){
+		 	echo "<".$children1->getName().">";
+		 		foreach ($children1->children() as $children2) {
+		 			$nama = $children2.getName();
+		 			if ($nama =='TITLE' OR $nama =='ARTIST' OR $nama =='YEAR'){
+		 				echo "<".$children2->getName().">".$children2."</".$children2->getName().">";
+		 			}
+		 		}
+		 	echo "</".$children1->getName().">";
+		}
+		echo "</".$xmlfile->getName().">";
+	}
+	 
+ ?>
